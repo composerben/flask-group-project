6b964a95c6615311def8ca42a13503210a70ebc9
composerben <56130322+composerben@users.noreply.github.com>
2021-06-25 14:07:57 -0700

    almost done with styling

--- react-app/src/components/UserPost/UserPost.js
+++ react-app/src/components/UserPost/UserPost.js
@@ -4,19 +4,20 @@ import { editOnePost } from "../../store/post";
 import "./user-post.css";
 
 export default function UserPostForm({ post }) {
-  const [caption, setCaption] = useState(post.caption);
+  const [caption, setCaption] = useState("");
   const dispatch = useDispatch();
 
   const onEdit = async (e) => {
     e.preventDefault();
     await dispatch(editOnePost(post.id, caption));
+    setCaption("");
   };
 
   return (
-    <form onSubmit={onEdit}>
-      <input
-        type="textarea"
+    <form className="form-caption" onSubmit={onEdit}>
+      <textarea
         value={caption}
+        placeholder="Edit Caption"
         onChange={(e) => setCaption(e.target.value)}
       />
       <button type="submit">Submit Edit</button>
